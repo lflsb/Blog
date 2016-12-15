26bc187148d756a0bad618d1b4759e20685ae0d9
lflsb <619713140@qq.com>
2016-12-15 21:56:37 +0800

    修改tag

--- js/tag.js
+++ js/tag.js
@@ -1,9 +1,33 @@
+var i = 0;
 $(function(){
     
     var tagsingle = $(".tags_single a");
-    alert(tagsingle);
-    for(var i =0 ;i<tagsingle.length;i++){
-        console.info($(tagsingle[i]).text());
-    }
+    var arr = tag_gettop(tagsingle);
+    
+//    for(var i = 0 ;i <arr.length ;i++){
+//        alert($(arr[i]).offset().top);
+//        arr[i].after($(".tags_single_content")[0]);
+//    }
+    
+    
+    
+    tagsingle.click(function(){
+        $(this).next().slideToggle(600);
+        
+    });
+    
     
 })
+
+function tag_gettop(tagsingle){
+    var arr = new Array();
+    for(var i =0 ;i<tagsingle.length;i++){
+        var currenttop = $(tagsingle[i]).offset().top;
+        if(i != 0 ){
+            if(currenttop != $(tagsingle[i-1]).offset().top){
+                arr.push(tagsingle[i-1]);
+            }
+        }
+    }
+    return arr;
+}
