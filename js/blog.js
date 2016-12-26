95bd25d32b42e0a110d29eea8fb9ac3160beeb33
lflsb <619713140@qq.com>
2016-12-27 01:24:13 +0800

    页面效果优化

--- js/blog.js
+++ js/blog.js
@@ -13,14 +13,8 @@ $(function(){
     //        $( ".blog_right" ).show( "drop", "slow" );
     //        $(this).addClass("opened");
     //    }
-        if($(this).hasClass("icon-xitongcaidan1")){
             $(this).toggleClass("icon-xitongcaidan1","1000");
             $(this).toggleClass("icon-guanbi","1000");
-            
-        }else if($(this).hasClass("icon-guanbi")){
-            $(this).toggleClass("icon-guanbi","1000");
-            $(this).toggleClass("icon-xitongcaidan1","1000");
-        }
         var buttonposition = $(this).position();
         var buttonleft = buttonposition.left-255;
         var buttontop = buttonposition.top - 5;
